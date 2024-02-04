c8ce75621e42195e81ccac56908d274070be7765
yaal96 <yarikaleksandro@yandex.ru>
2024-02-04 15:30:44 +0300

    Exceptions and logger

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import find_packages, setup
+from typing import list
 
 
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replce('\n', '') for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
 setup(
 name='mlproject',
@@ -10,5 +19,4 @@ author='yaal96',
 author_email='yarikaleksandro@yandex.ru',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
 )
